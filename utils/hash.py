b316e7163902b1af87fb466d320f6e41f3b7d646
TestProgger <gamersteam_dubki@mail.ru>
2023-01-16 07:41:18 +0000

    Created global variable BLOCK_SIZE responsible for the number of bytes that will be read from the file at a time

--- utils/hash.py
+++ utils/hash.py
@@ -1,12 +1,14 @@
 import xxhash
 
+BLOCK_SIZE = 2**20
+
 
 def calculate_hashsum(_path: str) -> str:
     xxh128 = xxhash.xxh128()
 
     with open(_path, 'rb') as rb:
         while True:
-            data = rb.read(65536)
+            data = rb.read(BLOCK_SIZE)
             if data:
                 xxh128.update(data)
             else:
